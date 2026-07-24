f2871d4a7a52915bf289177cfbe9a1dedb54418b
Frederick Wright <fredrhys@gmail.com>
2026-07-24 13:17:27 +0100

    Added modular addition and subtraction functions.

--- montmul.h
+++ montmul.h
@@ -16,6 +16,8 @@ ModEntry makeModEntry(uint64_t modulus, uint64_t neginv, uint64_t auxmodsq, uint
 ModEntry primeModEntry(uint64_t prime);
 ModEntry combineCoprimeModEntries(ModEntry operand1, ModEntry operand2);
 ModEntry increasePrimeModEntryPower(ModEntry powerEntry, ModEntry primeEntry);
+uint64_t addmod(uint64_t augend, uint64_t addend, uint64_t modulus);
+uint64_t submod(uint64_t minuend, uint64_t subtrahend, uint64_t modulus);
 uint64_t montmul(uint64_t multiplier, uint64_t multiplicand, ModEntry modEntry);
 uint64_t intermul(uint64_t multiplier, uint64_t multiplicand, ModEntry modEntry);
 uint64_t montexp(uint64_t base, uint64_t exponent, ModEntry modEntry);
