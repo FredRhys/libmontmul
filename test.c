bb20c6d1480c07bca219cc35a1e7d6b7f4b236f2
Frederick Wright <fredrhys@gmail.com>
2026-08-30 12:24:34 +0100

    Sqrt passes tests.

--- test.c
+++ test.c
@@ -160,17 +160,20 @@ void testNT(void) {
 
 void testSqrt(void) {
     {
-        ModEntry modEntry = makeModEntry(7, 10540996613548315209ULL, 4, 6);;
-        assert(sqrtmod(1, modEntry) == 1);
-        assert(sqrtmod(2, modEntry) == 3);
-        assert(sqrtmod(4, modEntry) == 2);
-        assert(sqrtmod(9, modEntry) == 3);
+        ModEntry modEntry = makeModEntry(7, 10540996613548315209ULL, 4, 6);
+        assert(montexp(sqrtmod(1, modEntry), 2, modEntry) == 1);
+        assert(montexp(sqrtmod(2, modEntry), 2, modEntry) == 2);
+        assert(montexp(sqrtmod(4, modEntry), 2, modEntry) == 4);
+        assert(montexp(sqrtmod(9, modEntry), 2, modEntry) == 2);
     }
     {
-        ModEntry modEntry = makeModEntry(13, 12770822820260458811ULL, 9, 12);;
-        assert(sqrtmod(1, modEntry) == 1);
-        fprintf(stdout, "%"PRIu64"\n", sqrtmod(10, modEntry));
-        assert(sqrtmod(10, modEntry) == 6);
+        ModEntry modEntry = makeModEntry(13, 12770822820260458811ULL, 9, 12);
+        assert(montexp(sqrtmod(1, modEntry), 2, modEntry) == 1);
+        assert(montexp(sqrtmod(3, modEntry), 2, modEntry) == 3);
+        assert(montexp(sqrtmod(4, modEntry), 2, modEntry) == 4);
+        assert(montexp(sqrtmod(9, modEntry), 2, modEntry) == 9);
+        assert(montexp(sqrtmod(10, modEntry), 2, modEntry) == 10);
+        assert(montexp(sqrtmod(12, modEntry), 2, modEntry) == 12);
     }
 }
 
